5b8ea01dbcb57e88954e7848a751b934ea9b57fa
iambharatchavhan <bharatchavhan141@gmail.com>
2024-03-18 22:16:23 +0530

    login and signup done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,16 @@ import Navbar from './components/common/Navbar'
 import Signup from './Pages/Signup'
 import Login from './Pages/Login'
 import OpenRoute from "./components/core/Auth/OpenRoute"
+import ForgotPassword from './Pages/ForgotPassword'
+import VerifyEmail from './Pages/VerifyEmail'
+import UpdatePassword from './Pages/UpdatePassword'
 
 function App() {
 
 
   return (
   <div className='w-screen min-h-screen bg-richblack-900 flex flex-col font-inter'>
-      <Navbar/>
+   <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route
@@ -31,7 +34,35 @@ function App() {
             </OpenRoute>
           }
         />
+
+    <Route
+          path="forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword />
+            </OpenRoute>
+          }
+        />  
+
+      <Route
+          path="verify-email"
+          element={
+            <OpenRoute>
+              <VerifyEmail />
+            </OpenRoute>
+          }
+        />  
+
+    <Route
+          path="update-password/:id"
+          element={
+            <OpenRoute>
+              <UpdatePassword />
+            </OpenRoute>
+          }
+        />  
     </Routes>
+
   </div>
   )
 }
